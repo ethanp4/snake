c19452a494f3f5c21bf5e32dd556727873be6a21
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-09-02 12:37:25 -0600

    init

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+using namespace std;
+
+struct Vector2 {
+  float x;
+  float y;
+};
+#define UP (Vector2) {0,1}
+#define DOWN (Vector2) {0,-1}
+#define LEFT (Vector2) {-1,0}
+#define RIGHT (Vector2) {1,0}
+
+int main() {
+  const Vector2 res = { 640,480 };
+
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  SDL_Event e;
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_CreateWindowAndRenderer(res.x, res.y, 0, &window, &renderer);
+
+  bool running = true;
+
+  while (running) {
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+        //exit 
+        case SDL_QUIT:
+          running = false;
+        break;
+
+        //keypress
+        case SDL_KEYDOWN:
+          switch(e.key.keysym.sym) {
+            case SDLK_UP:
+
+            break;
+            case SDLK_DOWN:
+            break;
+            case SDLK_LEFT:
+            break;
+            case SDLK_RIGHT:
+            break;
+          }
+
+      }
+    }
+    SDL_Delay(10);
+  }
+}
