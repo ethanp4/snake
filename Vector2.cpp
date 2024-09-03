7f2fbc42ef35c6968deb5e3b05624f7e6c6be230
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-09-02 21:24:18 -0600

    food and stuff

--- Vector2.cpp
+++ Vector2.cpp
@@ -2,8 +2,8 @@
 #include <sstream>
 
 // initialise constants
-const Vector2 Vector2::UP(0,1);
-const Vector2 Vector2::DOWN(0,-1);
+const Vector2 Vector2::UP(0,-1);
+const Vector2 Vector2::DOWN(0,1);
 const Vector2 Vector2::LEFT(-1,0);
 const Vector2 Vector2::RIGHT(1,0);
 
@@ -40,7 +40,7 @@ bool Vector2::operator!=(const Vector2& other) const {
   return !(x == other.x && y == other.y);
 }
 
-//iostream
+//tostring
 std::string Vector2::toString() const {
   std::ostringstream oss;
   oss << "(" << x << ", " << y << ")";
--- main.cpp
+++ main.cpp
@@ -1,50 +1,89 @@
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
+#include <chrono>
+#include <random>
 #include <iostream>
 #include <SDL2/SDL.h>
 #include "Vector2.h"
+
 using namespace std;
 
-enum DIRECTION {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
-};
-
-const Vector2 directions[] = {
-  {0,-1},//DOWN
-  {0,1}, //UP
-  {-1,0},//LEFT
-  {1,0}  //RIGHT
-};
-
-int movementDir;
+Vector2 movementDir;
 bool running;
 Vector2 playerPos;
 
-void move_player() {
-  auto delta = directions[movementDir];
-  playerPos = playerPos + delta;
+enum objects { NONE, PLAYER, FOOD };
+const Vector2 res = { 40,40 };
+int playField[40][40];
+
+random_device dev;
+mt19937 rng(dev());
+uniform_int_distribution<mt19937::result_type> distarea(1,res.x*res.y);
+uniform_int_distribution<mt19937::result_type> distlen(1, res.x);
+
+Uint32 move_player(Uint32 i, void *param) {
+  playField[playerPos.x][playerPos.y] = NONE;
+  playerPos = playerPos + movementDir;
+  playField[playerPos.x][playerPos.y] = PLAYER;
+  // cout << playerPos.toString() << endl;
+  return i;
+}
+
+Uint32 spawn_food(Uint32 i, void *param) {
+  Vector2 pos = {(int)distlen(rng), (int)distlen(rng)};
+  playField[pos.x][pos.y] = FOOD;
+  cout << "Spawning food at " << pos.toString() << endl;
+  return i;
+}
+
+void draw_pixels(SDL_Renderer &renderer) {
+  SDL_Colour colour;
+  for (int i = 0; i < res.x; i++) {
+    for (int j = 0; j < res.y; j++){
+      switch (playField[i][j]) {
+        case NONE:
+          continue;
+        break;
+        case PLAYER:
+          colour = {255,255,255,255};
+        break;
+        case FOOD:
+          colour = {26, 209, 180, 255};
+        break;      
+      }
+      SDL_SetRenderDrawColor(&renderer, colour.r, colour.g, colour.b, colour.a);
+      SDL_RenderDrawPoint(&renderer, i, j);
+    }
+  }
 }
 
 int main() {
   SDL_Init(SDL_INIT_EVERYTHING);
-  const Vector2 res = { 100,100 };
-  // create 600x600 window, which is actually 100x100 pixels scaled by 6x
-  SDL_Window* window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, res.x*6, res.y*6, 0);
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_RenderSetScale(renderer,6,6);
+  auto window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, res.x*16, res.y*16, 0);
+  auto renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_RenderSetScale(renderer,16,16);
   SDL_Event e;
 
+  for (int i = 0; i < res.x; i++) {
+    for (int j = 0; j < res.y; j++) {
+      //approximately 10 will begin with food
+      if (distarea(rng) < 10){
+        playField[i][j] = FOOD;
+      } else {
+        playField[i][j] = NONE;
+      }
+      // cout << playField[i][j];
+    }
+    // cout << endl;
+  }
   movementDir = 1;
   running = true;
   playerPos = (Vector2) {res.x/2,res.y/2};
   printf("Running\n");
 
-  // Vector2 a = Vector2(5,2);
-  // Vector2 b = Vector2(2,2);
-  // Vector2 r = a+b;
-  // printf("%s + %s = %s\n", a.toString().c_str(), b.toString().c_str(), r.toString().c_str());  
+  auto moveTimer = SDL_AddTimer(100, move_player, NULL);
+  auto spawnFoodTimer = SDL_AddTimer(5000, spawn_food, NULL);
 
   while (running) {
     while (SDL_PollEvent(&e)) {
@@ -62,30 +101,29 @@ int main() {
             break;
             //movement
             case SDLK_UP:
-              movementDir = UP;
+              movementDir = Vector2::UP;
             break;
             case SDLK_DOWN:
-              movementDir = DOWN;
+              movementDir = Vector2::DOWN;
             break;
             case SDLK_LEFT:
-              movementDir = LEFT;
+              movementDir = Vector2::LEFT;
             break;
             case SDLK_RIGHT:
-              movementDir = RIGHT;
+              movementDir = Vector2::RIGHT;
             break;
           }
-          printf("Movement direction: %i\n", movementDir);
+          // cout << "Movement dir is " << movementDir.toString() << endl;
         break;
       }
     }
-    move_player();
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDrawPoint(renderer, playerPos.x, playerPos.y);
-    
+    draw_pixels(*renderer);
+
     SDL_RenderPresent(renderer);
-    SDL_Delay(100);
+
+    SDL_Delay(10);
   }
 }
