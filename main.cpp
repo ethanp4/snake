8832f03d1ab4c9509bf30160d1869ca824ea8d1f
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-09-06 22:40:15 -0600

    working text and score

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_ttf.h"
-// #include <SDL2/SDL_render.h>
-// #include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_render.h>
+#include <cstddef>
 #include <stdio.h>
 #include <random>
 #include <iostream>
@@ -20,17 +20,28 @@ Vector2 playerPos;
 int foodCount = 0;
 int playerLength = 0;
 vector<Vector2> positionHistory;
-const Vector2 res = { 40,40 };
-int playField[40][40];
+
+//constants 
+const int res = 600;
+const int resScale = 1;
+const int pixelSize = 18;
+const int playFieldSize = res / pixelSize;
+int playField[playFieldSize][playFieldSize];
+
+const int minFood = 7;
+const int approxStartingFood = 7;
 
 vector<SDL_TimerID> timers;
 
 //rng stuff
 random_device dev;
 mt19937 rng(dev());
-uniform_int_distribution<mt19937::result_type> distdir(0,3);
-uniform_int_distribution<mt19937::result_type> distarea(1,res.x*res.y);
-uniform_int_distribution<mt19937::result_type> distlen(1, res.x);
+uniform_int_distribution<mt19937::result_type> distdir(0, 3);
+uniform_int_distribution<mt19937::result_type> distarea(1, playFieldSize*playFieldSize);
+uniform_int_distribution<mt19937::result_type> distlen(1, playFieldSize);
+
+TTF_Font* bigFont;
+TTF_Font* littleFont;
 
 void quit_game() {
   running = false;
@@ -46,20 +57,22 @@ Uint32 move_player(Uint32 i, void *param) {
   //add the previous position to the start of the vector
   positionHistory.insert(positionHistory.begin(), playerPos);
 
+  //calculate future position then wrap around if necessary
   playerPos = playerPos + movementDir;
-  if (playerPos.x > 39) {
+  if (playerPos.x > playFieldSize-1) {
     playerPos.x = 0;
   }
   if (playerPos.x < 0) {
-    playerPos.x = 39;
+    playerPos.x = playFieldSize-1;
   }
-  if (playerPos.y > 39) {
+  if (playerPos.y > playFieldSize-1) {
     playerPos.y = 0;
   }
   if (playerPos.y < 0) {
-    playerPos.y = 39;
+    playerPos.y = playFieldSize-1;
   }
 
+  //switch for collision detection
   switch (playField[playerPos.x][playerPos.y]) {
     case NONE:
     break;
@@ -101,14 +114,14 @@ Uint32 spawn_food(Uint32 i, void *param) {
       cout << "Foodcount is " << foodCount << endl;
     }
     //if food is low then spawn more
-  } while (foodCount < 7);
+  } while (foodCount < minFood);
   return i;
 }
 
 void draw_pixels(SDL_Renderer &renderer) {
   SDL_Colour colour;
-  for (int i = 0; i < res.x; i++) {
-    for (int j = 0; j < res.y; j++){
+  for (int i = 0; i < playFieldSize; i++) {
+    for (int j = 0; j < playFieldSize; j++){
       switch (playField[i][j]) {
         case NONE:
           continue;
@@ -125,34 +138,57 @@ void draw_pixels(SDL_Renderer &renderer) {
         break;      
       }
       SDL_SetRenderDrawColor(&renderer, colour.r, colour.g, colour.b, colour.a);
-      SDL_RenderDrawPoint(&renderer, i, j);
+      SDL_Rect pixel = {i*pixelSize, j*pixelSize, pixelSize, pixelSize};
+      SDL_RenderFillRect(&renderer, &pixel);
+      // SDL_RenderDrawPoint(&renderer, i, j);
     }
+  }
+}
+
+void render_text(SDL_Renderer &renderer) {
+  SDL_Surface* scoreSurface = TTF_RenderText_Solid(littleFont, to_string(playerLength).c_str(), {255,255,255});
+  SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(&renderer, scoreSurface);
+  int scoreTexW, scoreTexH;
+  SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreTexW, &scoreTexH);
+  SDL_Rect scoreRect = { 0, 0, scoreTexW, scoreTexH};
+  SDL_RenderCopy(&renderer, scoreTexture, NULL, &scoreRect);
+  
+  if (gameOver) {
+    SDL_Surface* gameoverSurface = TTF_RenderText_Solid(bigFont, "Gameover", {255,255,255});
+    SDL_Texture* gameoverTexture = SDL_CreateTextureFromSurface(&renderer, gameoverSurface);
+    int texW, texH;
+    SDL_QueryTexture(gameoverTexture, NULL, NULL, &texW, &texH);
+    SDL_Rect gameoverRect = { (res-texW)/2, res/2-texH, texW, texH };
+
+    SDL_Surface* infoSurface = TTF_RenderText_Solid(littleFont, "Press space to restart", {255,255,255});
+    SDL_Texture* infoTexture = SDL_CreateTextureFromSurface(&renderer, infoSurface);
+    int infoTexW, infoTexH;
+    SDL_QueryTexture(infoTexture, NULL, NULL, &infoTexW, &infoTexH);
+    SDL_Rect infoRect = { (res-infoTexW)/2, res/2, infoTexW, infoTexH };
+
+    SDL_RenderCopy(&renderer, gameoverTexture, NULL, &gameoverRect);
+    SDL_RenderCopy(&renderer, infoTexture, NULL, &infoRect);
   }
 }
 
 int main() {
   //initialize sdl stuff
   SDL_Init(SDL_INIT_EVERYTHING);
-  auto window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, res.x*16, res.y*16, 0);
+  auto window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, res*resScale, res*resScale, 0);
   auto renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_RenderSetScale(renderer,16,16);
+  SDL_RenderSetScale(renderer,resScale,resScale);
   SDL_Event e;
 
   //text
-  TTF_Font* font = TTF_OpenFont("fonts/Tiny5-Regular.ttf", 7);
-  SDL_Surface* gameoverSurface = TTF_RenderText_Solid(font, "Gameover", {255,255,255});
-  SDL_Texture* gameoverTexture = SDL_CreateTextureFromSurface(renderer, gameoverSurface);
-  SDL_Rect gameoverRect;
-  gameoverRect.x = 0;
-  gameoverRect.y = 0;
-  gameoverRect.w = 40;
-  gameoverRect.h = 40;
+  TTF_Init();
+  bigFont = TTF_OpenFont("./fonts/Tiny5-Regular.ttf", 72);
+  littleFont = TTF_OpenFont("./fonts/Tiny5-Regular.ttf", 36);
+
 
   //initialize playfield
-  for (int i = 0; i < res.x; i++) {
-    for (int j = 0; j < res.y; j++) {
-      //approximately 10 will begin with food
-      if (distarea(rng) < 7){
+  for (int i = 0; i < playFieldSize; i++) {
+    for (int j = 0; j < playFieldSize; j++) {
+      if (distarea(rng) < approxStartingFood){
         playField[i][j] = FOOD;
         foodCount++;
       } else {
@@ -231,13 +267,13 @@ int main() {
 
     //draw objects
     draw_pixels(*renderer);
-    SDL_RenderCopy(renderer, gameoverTexture, NULL, &gameoverRect);
+    render_text(*renderer);
+
     if (gameOver) { 
       movementDir = {0,0}; 
-      // SDL_FreeSurface(gameoverSurface);
-      // SDL_DestroyTexture(gameoverTexture);
     }
     SDL_RenderPresent(renderer);
     SDL_Delay(10);
   }
 }
+
