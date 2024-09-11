567ee006c01e87999eb5d650a7a24749c9db45e0
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-09-10 23:01:53 -0600

    server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 
-app = Flask(__name__, template_folder="html", static_folder="static")
+app = Flask(__name__, template_folder="html")
 
 scores = []
 idCount = 0
@@ -17,18 +17,16 @@ def root():
                 """
   return render_template("leaderboard.html", scoresList=html)
 
-@app.route("/leaderboard", methods=['GET'])
-def getLeaderboard():
-  return jsonify(scores), 200
+@app.route("/leaderboard", methods=['GET', 'POST'])
+def leaderboard():
+  if request.method == 'GET':
+    return jsonify(scores), 200
 
-@app.route("/leaderboard", methods=['POST'])
-def postScore():
-  global idCount
   score = request.get_json()["score"]
-  
   if score is None:
     return "Bad request: no score given", 400
 
+  global idCount
   idCount += 1
   id = idCount
   score = {
